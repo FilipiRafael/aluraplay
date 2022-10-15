d4009df4b6d0ac4ee256eab78f6c5207e90d8cb9
Filipi Rafael <filipirafael.123@gmail.com>
2022-10-15 19:51:49 -0300

    Add project finished

---
+++ js/mostrarVideos.js
@@ -0,0 +1,34 @@
+const lista = document.querySelector("[data-lista]");
+
+import { conectaApi } from "./conectaApi.js";
+
+export default function constroiCard(title, description, url, image) {
+    const video = document.createElement("li");
+    video.className = "videos__item";
+    video.innerHTML = 
+    `
+    <iframe width="100%" height="72%" src="${url}"
+    title="${title}" frameborder="0"
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    allowfullscreen></iframe>
+    <div class="descricao-video">
+        <img src="${image}" alt="logo canal alura">
+        <h3>${title}</h3>
+        <p>${description}</p>
+    </div>
+    `
+    return video;
+}
+
+async function listaVideos() {
+    try {
+        const listaApi = await conectaApi.listaVideos();
+        listaApi.forEach((elemento) => lista.appendChild(
+            constroiCard(elemento.titulo, elemento.descricao, elemento.url, elemento.imagem
+        )));
+    } catch {
+        lista.innerHTML = `<h2 class="mensagem__titulo">Não foi possível carregar a lista de vídeos </h2>`
+    }
+}
+
+listaVideos();
